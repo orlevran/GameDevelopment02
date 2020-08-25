0d4c3888789f683f2fd139cff1cd48ccc368c479
orlevran <orlevor@gmail.com>
2020-08-26 01:49:45 +0300

    HW2

---
+++ HouseModel001/Assets/Scripts/CameraMotion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class CameraMotion : MonoBehaviour
+{
+    private float _speed = 0.08f, _rotation;
+    private Vector3 _direction;
+    private CharacterController _characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this._direction = Vector3.zero;
+        this._characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this._rotation = Input.GetAxis("Horizontal") * (float)Math.PI * 10f;
+
+        if (Input.GetKey(KeyCode.UpArrow) && this._speed <= 1.5f)
+        {
+            this._speed += 0.1f;
+            this._direction = Vector3.forward;
+        }
+        /*
+        else if (Input.GetKey(KeyCode.DownArrow) && this._speed >= -1.5f)
+        {
+            this._speed -= 0.1f;
+            this._direction = Vector3.back;
+        }*/
+        transform.Rotate(0, this._rotation * Time.deltaTime * this._speed, 0);
+        this._characterController.Move(transform.TransformDirection(this._direction * Time.deltaTime * this._speed));
+    }
+}
---
+++ HouseModel001/Assets/Scripts/FrontDoorMotion.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrontDoorMotion : MonoBehaviour
+{
+    public Animator animator;
+    public bool isOpen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isOpen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            animator.SetTrigger("Open");
+            isOpen = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(isOpen)
+        {
+            animator.SetTrigger("Close");
+            isOpen = false;
+        }
+    }
+}
